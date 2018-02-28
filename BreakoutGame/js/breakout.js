0a1c48f86d4f5191fc76563de2263256af392333
keighlum <35769357+keighlum@users.noreply.github.com>
2018-02-28 13:40:23 +0000

    v4
    ball bounces off walls now

--- BreakoutGame/js/breakout.js
+++ BreakoutGame/js/breakout.js
@@ -5,10 +5,12 @@ var x=canvas.width/2;
 var y=canvas.height-30;
 var dx=2;
 var dy=-2;
+var ballRadius=10;
+
 
 function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle="#0095DD";
 	ctx.fill();
 	ctx.closePath();
@@ -16,8 +18,17 @@ function drawBall() {
 function draw(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+	
+	if(x+dx>canvas.width-ballRadius||x+dx<ballRadius){
+		dx=-dx;
+	}
 	x += dx;
 	y += dy;
+	
+	if(y+dy>canvas.height-ballRadius||y+dy<ballRadius){
+		dy=-dy;
+	}
+	
 }
 
 setInterval(draw, 10);
