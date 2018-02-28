a5b395827bc7d32a35744a479683da0036e97211
keighlum <35769357+keighlum@users.noreply.github.com>
2018-02-28 14:22:24 +0000

    v6
    added paddle and it can move left and right

--- BreakoutGame/js/breakout.js
+++ BreakoutGame/js/breakout.js
@@ -1,6 +1,13 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var PaddleHeight=10;
+var PaddleWidth=75;
+var PaddleX=(canvas.width-PaddleWidth)/2;
+
+var rightPressed=false;
+var leftPressed=false;
+
 var x=canvas.width/2;
 var y=canvas.height-30;
 var dx=2;
@@ -13,27 +20,63 @@ var ballColour="#0095DD";
 function drawBall() {
 	ctx.beginPath();
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-	ctx.fillStyle=ballColour;
+	ctx.fillStyle="#0095DD";
 	ctx.fill();
 	ctx.closePath();
 }
+
+function drawPaddle(){
+	ctx.beginPath();
+	ctx.rect(PaddleX, canvas.height-PaddleHeight, PaddleWidth, PaddleHeight);
+	ctx.fillStyle="#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
 function draw(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
-	
+	drawPaddle();
 	if(x+dx>canvas.width-ballRadius||x+dx<ballRadius){
 		dx=-dx;
-		ballColour="yellow";
+	
 	}
 	x += dx;
 	y += dy;
 	
 	if(y+dy>canvas.height-ballRadius||y+dy<ballRadius){
 		dy=-dy;
-		ballColour="red";
+
 	}
+	if(rightPressed && PaddleX<canvas.width-PaddleWidth){
+	PaddleX+=7;
+}
+else if(leftPressed && PaddleX>0){
+	PaddleX-=7;
+}
 	
 }
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e){
+	if(e.keyCode==39){
+		rightPressed=true;
+	}
+	else if(e.keyCode==37){
+		leftPressed=true;
+	}
+}
+
+function keyUpHandler(e){
+	if(e.keyCode==39){
+		rightPressed=false;
+	}
+	else if(e.keyCode==37){
+		leftPressed=false;
+	}
+}
+
 
 setInterval(draw, 10);
 
